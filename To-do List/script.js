15dcffa29fe52aaf591c8ed43699885b4fcd1f5e
nidhiswar0 <69695837+nidhiswar0@users.noreply.github.com>
2024-08-30 00:17:57 +0530

    Update script.js

--- To-do List/script.js
+++ To-do List/script.js
@@ -13,19 +13,16 @@ function displayTodo() {
   let todoListHTML = ''
   for (let i =0 ; i <todoList.length ; i++) {
     let tasksObj = todoList[i];
-    const {name ,date} = todoObj; 
-    const html = `
-    <p>
-      ${name}   ${date}
-      <button onclick='todoList.splice(${i},1);displayTodo();'>
-       Delete
-      </button>
-
-    </p>`;
+    const {name ,date} = tasksObj; 
+    const html = 
+    <div>${name}</div>
+    <div>${date}</div>
+    <button onclick='todoList.splice(${i},1);displayTodo();'>
+      Delete
+    </button>
+    ;
     todoListHTML += html
   }
   console.log(todoListHTML);
   document.querySelector('.js-todo-row-input').innerHTML = todoListHTML
 }
-
-
