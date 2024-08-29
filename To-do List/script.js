20a0b15025cbc110807686fef9a22172e8186c25
nidhiswar0 <69695837+nidhiswar0@users.noreply.github.com>
2024-08-30 00:41:36 +0530

    Update script.js

--- To-do List/script.js
+++ To-do List/script.js
@@ -1,28 +1,27 @@
-todoList= [] ; 
-
+const todoList = [];
 
 function addTodo() {
   const inputElement = document.querySelector('.js-input').value;
   const inputDate = document.querySelector('.js-date').value;
-  const inputObject = {name:inputElement,date:inputDate};
+  const inputObject = { name: inputElement, date: inputDate };
   todoList.push(inputObject);
   displayTodo();
 }
 
 function displayTodo() {
-  let todoListHTML = ''
-  for (let i =0 ; i <todoList.length ; i++) {
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
     let tasksObj = todoList[i];
-    const {name ,date} = tasksObj; 
-    const html = 
-    <div>${name}</div>
-    <div>${date}</div>
-    <button onclick='todoList.splice(${i},1);displayTodo();'>
-      Delete
-    </button>
-    ;
-    todoListHTML += html
+    const { name, date } = tasksObj;
+    const html = `
+      <div class="todo-item">
+        <span>${name} - ${date}</span>
+        <button onclick='todoList.splice(${i}, 1); displayTodo();'>
+          Delete
+        </button>
+      </div>
+    `;
+    todoListHTML += html;
   }
-  console.log(todoListHTML);
-  document.querySelector('.js-todo-row-input').innerHTML = todoListHTML
+  document.querySelector('.js-todo-row-input').innerHTML = todoListHTML;
 }
